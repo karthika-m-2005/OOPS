4b559563de17c3dbcb9f80efd1608ca965b9ffd7
KARTHIKA M <km646383@gmail.com>
2025-02-11 10:24:23 +0530

    Char Stack

---
+++ chardemo.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class chardemo {
+	
+      public static void main(String[] args) {
+		
+    	  StackChar s1 = new StackChar();
+    	  s1.push('K');
+    	  s1.push('A');
+    	  s1.push('R');
+    	  s1.push('T');
+    	  s1.push('H');
+    	  s1.push('I');
+    	  s1.push('K');
+    	  s1.push('A');
+    	  
+    	  System.out.println(s1.pull());
+    	  
+    	  
+    	  
+
+	}
+
+
+}
